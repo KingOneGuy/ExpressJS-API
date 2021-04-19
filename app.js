0c9041ef2155d4b6778e722320c2acfdd5982b79
Emil <systemskog@gmail.com>
2021-04-19 04:49:18 -0700

    /helloworld + /helloworldjson

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express') 
 const app = express() 
 const port = 3000 
+
+let users = [];
  
-app.get('/', (req, res) => { 
-    res.send('Hello World!') 
+app.get('/helloworld', (req, res) => { 
+    res.send('hello world') 
 })
 
-app.get('/user', (req, res) => { 
-    res.send('User Get') 
+app.get('/helloworldjson', (req, res) => { 
+    res.send({"helloworldjson":"hello world"}) 
 })
 
 app.post('/', (req, res) => {
